c3c25ccf8cc396e39af6ef1c08785a4fc127e728
Duy.pc <le.pham@outlook.de>
2024-01-18 22:31:10 +0100

    slight change

--- code_challenge/app.js
+++ code_challenge/app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+const survey = require('./challenge.answers.json')
 
 const port = 5000;
 
@@ -13,9 +14,14 @@ app.get('/', (req,res)=>{
     res.send('Hello Test')
 })
 
+app.get('/survey',(req,res)=>{
+    res.json(survey)
+})
+
+
 
 // GET /scores 
 app.get('/scores',(req,res)=>{
     //code zu schreiben
-    res.json({femaleScore: filler1, maleScore: filler2, diverseScore:filler3})
+    res.json({femaleScore: 'filler1', maleScore: 'filler2', diverseScore: 'filler3'})
 }) 
