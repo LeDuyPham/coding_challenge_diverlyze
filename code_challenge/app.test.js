31df156676b94baa1953fe022dbf36d62d7cd3f1
Duy.laptop <le.pham@outlook.de>
2024-01-19 16:44:45 +0100

    added test files, incomplete

---
+++ code_challenge/app.test.js
@@ -0,0 +1,7 @@
+const calc = require('./score_calc')
+const answers = require('./test_answers')
+
+
+test('add', () => {
+    expect(3).toBe(3)
+})
---
+++ code_challenge/test_answers.js
@@ -0,0 +1,60 @@
+//Hier befinden sich alle Testarrays
+
+/*
+Es wird auf die Anzahl der Surveys getestet
+Die folgenden Kodierung der Namen ist wie folgt aufgebaut:
+ - fx_my_dz => female x times, male y times, diverse z times
+*/
+
+const f3m3d3 = [
+    {"_id": "64f35f1be1b807d66c78274e", "rating": 8, "gender": "female"},
+    {"_id": "64f5ec161777cc7cef3e0db8", "rating": 8,"gender": "female"},
+    {"_id": "64f5ecb61777cc7cef3e0e08","rating": 8,"gender": "female"},
+    {"_id": "64f5ecc01777cc7cef3e0e1a","rating": 9,"gender": "male"},
+    {"_id": "64f6fa8a20148efaf8ebf429","rating": 9,"gender": "male"},
+    {"_id": "64f6fb1220148efaf8ebf4ed","rating": 9,"gender": "male"},
+    {"_id": "64f6ff025e5b744d0818e599","rating": 10,"gender": "diverse"},
+    {"_id": "64f700095e5b744d0818e5df","rating": 10,"gender": "diverse"},
+    {"_id": "64f7006c5e5b744d0818e62c","rating": 10,"gender": "diverse"}]
+
+const f2m3d3 = [
+    {"_id": "64f35f1be1b807d66c78274e", "rating": 8, "gender": "female"},
+    {"_id": "64f5ec161777cc7cef3e0db8", "rating": 8,"gender": "female"},
+    {"_id": "64f5ecc01777cc7cef3e0e1a","rating": 9,"gender": "male"},
+    {"_id": "64f6fa8a20148efaf8ebf429","rating": 9,"gender": "male"},
+    {"_id": "64f6fb1220148efaf8ebf4ed","rating": 9,"gender": "male"},
+    {"_id": "64f6ff025e5b744d0818e599","rating": 10,"gender": "diverse"},
+    {"_id": "64f700095e5b744d0818e5df","rating": 10,"gender": "diverse"},
+    {"_id": "64f7006c5e5b744d0818e62c","rating": 10,"gender": "diverse"}]
+
+const f3m2d3 = [
+    {"_id": "64f35f1be1b807d66c78274e", "rating": 8, "gender": "female"},
+    {"_id": "64f5ec161777cc7cef3e0db8", "rating": 8,"gender": "female"},
+    {"_id": "64f5ecb61777cc7cef3e0e08","rating": 8,"gender": "female"},
+    {"_id": "64f6fa8a20148efaf8ebf429","rating": 9,"gender": "male"},
+    {"_id": "64f6fb1220148efaf8ebf4ed","rating": 9,"gender": "male"},
+    {"_id": "64f6ff025e5b744d0818e599","rating": 10,"gender": "diverse"},
+    {"_id": "64f700095e5b744d0818e5df","rating": 10,"gender": "diverse"},
+    {"_id": "64f7006c5e5b744d0818e62c","rating": 10,"gender": "diverse"}]
+
+const f3m3d2 = [
+    {"_id": "64f35f1be1b807d66c78274e", "rating": 8, "gender": "female"},
+    {"_id": "64f5ec161777cc7cef3e0db8", "rating": 8,"gender": "female"},
+    {"_id": "64f5ecb61777cc7cef3e0e08","rating": 8,"gender": "female"},
+    {"_id": "64f5ecc01777cc7cef3e0e1a","rating": 9,"gender": "male"},
+    {"_id": "64f6fa8a20148efaf8ebf429","rating": 9,"gender": "male"},
+    {"_id": "64f6fb1220148efaf8ebf4ed","rating": 9,"gender": "male"},
+    {"_id": "64f6ff025e5b744d0818e599","rating": 10,"gender": "diverse"},
+    {"_id": "64f700095e5b744d0818e5df","rating": 10,"gender": "diverse"}]
+
+const f2m2d2 = [
+    {"_id": "64f35f1be1b807d66c78274e", "rating": 8, "gender": "female"},
+    {"_id": "64f5ec161777cc7cef3e0db8", "rating": 8,"gender": "female"},
+    {"_id": "64f5ecc01777cc7cef3e0e1a","rating": 9,"gender": "male"},
+    {"_id": "64f6fa8a20148efaf8ebf429","rating": 9,"gender": "male"},
+    {"_id": "64f700095e5b744d0818e5df","rating": 10,"gender": "diverse"},
+    {"_id": "64f7006c5e5b744d0818e62c","rating": 10,"gender": "diverse"}]
+
+const f0m0d0 = []
+
+    module.exports = {f3m3d3,f2m3d3,f3m2d3,f3m3d2,f2m2d2,f0m0d0}
