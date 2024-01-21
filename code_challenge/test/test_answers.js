1391ebc8a876eca7c97f73e35829a5b572eb5b05
Duy.pc <le.pham@outlook.de>
2024-01-21 14:45:24 +0100

    tests comments updated, readme updated

--- code_challenge/test/test_answers.js
+++ code_challenge/test/test_answers.js
@@ -2,9 +2,8 @@
 
 /* 
 Die folgenden Kodierung der Namen ist wie folgt aufgebaut:
-    - sx_iy_avgz_a => surveys x times, invalid rating x times, average score of z_a
+    - sx_iy_avgz_a => surveys x times, invalid rating y times, average score of z_a
 */
-
 
 const s3_i0_avg5 = [
     {"_id": "64f35f1be1b807d66c78274e", "rating": 1, "gender": "female"},
@@ -101,16 +100,23 @@ const s6_f2m2d2 = [
     {"_id": "64f700095e5b744d0818e5df","rating": 10,"gender": "diverse"},
     {"_id": "64f7006c5e5b744d0818e62c","rating": 10,"gender": "diverse"}]
 
-
-const s4_f2m0d2 = [
+const s6_f6m0d0 = [
     {"_id": "64f35f1be1b807d66c78274e", "rating": 8, "gender": "female"},
     {"_id": "64f5ec161777cc7cef3e0db8", "rating": 8,"gender": "female"},
-    {"_id": "64f700095e5b744d0818e5df","rating": 10,"gender": "diverse"},
-    {"_id": "64f7006c5e5b744d0818e62c","rating": 10,"gender": "diverse"}]
+    {"_id": "64f5ecc01777cc7cef3e0e1a","rating": 9,"gender": "female"},
+    {"_id": "64f6fa8a20148efaf8ebf429","rating": 9,"gender": "female"},
+    {"_id": "64f700095e5b744d0818e5df","rating": 10,"gender": "female"},
+    {"_id": "64f7006c5e5b744d0818e62c","rating": 10,"gender": "female"}]
+
+const s4_f2m2d0 = [
+    {"_id": "64f35f1be1b807d66c78274e", "rating": 8, "gender": "female"},
+    {"_id": "64f5ec161777cc7cef3e0db8", "rating": 8,"gender": "female"},
+    {"_id": "64f700095e5b744d0818e5df","rating": 10,"gender": "male"},
+    {"_id": "64f7006c5e5b744d0818e62c","rating": 10,"gender": "male"}]
 
 const s0_f0m0d0 = []
 
 module.exports = {
-    s9_f3m3d3,s8_f2m3d3,s8_f3m2d3,s8_f3m3d2,s6_f2m2d2,s4_f2m0d2,s0_f0m0d0,
+    s9_f3m3d3,s8_f2m3d3,s8_f3m2d3,s8_f3m3d2,s6_f2m2d2,s6_f6m0d0,s4_f2m2d0,s0_f0m0d0,
     s3_i0_avg5,s2_i0_avg4_0,s9_i0_avg6_2,s3_i1_avg0,s4_i1_avg3_3,s9_i4_avg5_8
 }
